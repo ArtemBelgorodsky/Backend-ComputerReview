9ca162654d9653e4cd6fec7b951f407ce3ac6d4c
Wisdou <51473681+Wisdou@users.noreply.github.com>
2022-10-12 23:01:40 +0300

    Initialise repository

---
+++ Computer-Backend/Computer-Backend/Parsers/IShopParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Computer_Backend.Parsers
+{
+    interface IShopParser
+    {
+        IComputerInfo GetInfo(string href);
+        IComputerInfo GetInfo(Uri href);
+    }
+}
