4381580529dd83cba9355d8c9be09529a9fc783b
Wisdou <dlisay430@gmail.com>
2022-10-27 16:24:27 +0300

    Init yandex shop parser

--- Computer-Backend/Computer-Backend/Controllers/ComputerInfoController.cs
+++ Computer-Backend/Computer-Backend/Controllers/ComputerInfoController.cs
@@ -9,16 +9,16 @@ namespace Computer_Backend.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ComputerInfoController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<ComputerInfoController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public ComputerInfoController(ILogger<ComputerInfoController> logger)
         {
             _logger = logger;
         }
